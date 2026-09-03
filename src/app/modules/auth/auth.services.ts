6a1d62e1627c7c61e09d7d9947e3a5c8f3b9bb1f
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2026-09-03 19:08:23 +0600

    add customer fields and implement registration logic with email verification

--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -1,3 +1,66 @@
-import prisma from "../../lib/prisma";
+import { prisma } from "../../lib/prisma";
+import crypto from "crypto";
+import { redisClient } from "../../lib/redis";
+import bcrypt from "bcryptjs";
+import { sendEmail } from "../../utils/sendEmail";
+import config from "../../config";
+
+const registerCustomer = async (payload: any) => {
+  const { name, email, password, customerData } = payload;
+
+  const isUserExists = await prisma.users.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (isUserExists) {
+    throw new Error("User already exists");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 8);
+  const expirationSeconds = 5 * 60;
+  const otpKey = `register-customer-otp:${email}`;
+  const otpValue = crypto.randomInt(100000, 1000000).toString();
+
+  await redisClient.set(otpKey, otpValue, {
+    expiration: {
+      type: "EX",
+      value: expirationSeconds,
+    },
+  });
+
+  const customerRegistrationKey = `customer-registration-data:${email}`;
+  const customerRegistrationPayload = {
+    name,
+    email,
+    password: hashedPassword,
+    customer: customerData,
+  };
+
+  await redisClient.set(
+    customerRegistrationKey,
+    JSON.stringify(customerRegistrationPayload),
+    {
+      expiration: {
+        type: "EX",
+        value: expirationSeconds,
+      },
+    },
+  );
+
+  const templateData = {
+    name,
+    email,
+    otp: otpValue,
+    expirationMinutes: expirationSeconds / 60,
+  };
+
+  await sendEmail("verify-email.ejs", templateData, {
+    from: config.email_sender,
+    to: email,
+    subject: "Verify your email",
+  });
+};
 
 export const AuthServices = {};
